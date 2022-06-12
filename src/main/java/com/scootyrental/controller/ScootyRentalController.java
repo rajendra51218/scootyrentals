45fa1ddd4bdc1cce8b2017a93f604d7317d759f1
Rajendra <rajendraprasad.shenll@gmail.com>
2022-06-12 14:15:46 +0530

    comments added

--- src/main/java/com/scootyrental/controller/ScootyRentalController.java
+++ src/main/java/com/scootyrental/controller/ScootyRentalController.java
@@ -17,21 +17,25 @@ public class ScootyRentalController {
     @Autowired
     ScootyRentalService scootyRentalService;
 
+    // Get All outlets
     @GetMapping("/outlets")
     public List<Outlet> getOutlets() {
          return  scootyRentalService.getOutlets();
     }
 
+    // get outlets based on area otherwise nearest area
     @GetMapping("/outlets/{area}")
     public List<Outlet> getOutletsByAddress(@PathVariable String area) {
         return scootyRentalService.getOutletByAddress(area);
     }
 
+    //updating Status of Scooty like available and reserved
     @PutMapping("/scooty/status")
     public Scooty updateScootyStatus(@RequestBody UpdateScootyStatusReq req) {
         return scootyRentalService.updateScootyStatus(req);
     }
 
+    // checking avialable parking slots before parking the scooty
     @GetMapping("/availableParkingSlots/{area}")
     public List<Outlet> getAvailableParkingSlots(@PathVariable String area) {
          return scootyRentalService.getOutletParkingSlots(area);
